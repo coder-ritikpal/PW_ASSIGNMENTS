bd7f506675c85ce64694af8b947542b3d107dd1e
Ritik Pal <palritik156>
2023-01-03 12:09:45 +0530

    initial commit

---
+++ rectangleArea.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int length,breadth;
+ cout<<"Enter length and breadth:";
+ cin>>length>>breadth;
+
+ int Area;
+ Area=length*breadth;
+
+ cout<<"Area of rectangle is:"<<Area;
+
+ 
+return 0;
+}
