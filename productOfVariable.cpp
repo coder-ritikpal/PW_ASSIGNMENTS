7beceefce037804861812e4808e14e5293768bd8
Ritik Pal <palritik156>
2023-01-03 18:57:33 +0530

    Added file

---
+++ productOfVariable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+ int x,y;
+ cout<<"Enter the two values:";
+ cin>>x>>y;
+
+ int product;
+ product=x*y;
+
+ cout<<"Product is:"<<product;
+return 0;
+}
