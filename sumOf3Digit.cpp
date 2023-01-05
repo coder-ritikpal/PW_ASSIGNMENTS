84bc3da1e6f084f9d828371ae5362df0e6cfaa30
Ritik Pal <palritik156>
2023-01-05 18:10:44 +0530

    Completed 2 assignment

---
+++ sumOf3Digit.cpp
@@ -0,0 +1,19 @@
+//Applicable for all digit no.
+#include<iostream>
+using namespace std;
+int main(){
+    int no;
+    cout<<"Enter  no:";
+    cin>>no;
+
+    int sum=0,remainder;
+
+    while(no!=0){
+        remainder=no%10;
+        sum+=remainder;
+        no=no/10;
+
+    }
+    cout<<"Sum of no is:"<<sum;
+return 0;
+}
